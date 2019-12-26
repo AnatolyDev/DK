76c2f2a0b562900e6323ca676623e38a5b22aed2
Anatoly <tolik_83@mail.ru>
2019-12-26 23:29:41 +0300

    Сборка контейнера для backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export APP_SETTINGS="config.DevelopmentConfig"
-export DATABASE_URL='postgresql://user:12341234@172.17.0.1/stom'
+export DATABASE_URL='postgresql://postgres:12341234@172.17.0.1/dk'
 exec gunicorn -b :5000 --access-logfile - --error-logfile - manage:app
