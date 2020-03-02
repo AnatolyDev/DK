669da09e2321f7f03067763df28bbcc777d68426
Anatoly Korzakov <tolik_83@mail.ru>
2020-03-02 16:18:24 +0300

    Progress в билетах

--- frontend/src/containers/Dashboard/Tickets/index.js
+++ frontend/src/containers/Dashboard/Tickets/index.js
@@ -8,6 +8,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { ticketsAPI } from '../../../api';
 
@@ -24,53 +25,66 @@ const Tickets = () => {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const [rows, setRows] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(
         () => {
             async function getTicketsOfUser(userID) {
+                setLoading(true);
                 const tickets = await ticketsAPI.getTickets(userID);
                 setRows(tickets.tickets);
+                setLoading(false);
             }
             getTicketsOfUser(1);
-        }
+        },
+        []
     )
 
     return (
-        <Paper className='root'>
-            <TableContainer className='container'>
-                <Table stickyHeader aria-label='sticky table'>
-                <TableHead>
-                    <TableRow>
-                    {columns.map(column => (
-                        <TableCell
-                            key={column.id}
-                            align={column.align}
-                            style={{ minWidth: column.minWidth }}
-                        >
-                        {column.label}
-                        </TableCell>
-                    ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
-                    return (
-                        <TableRow hover role='checkbox' tabIndex={-1} key={row.code}>
-                        {columns.map(column => {
-                            const value = row[column.id];
-                            return (
-                            <TableCell key={column.id} align={column.align}>
-                                {column.format && typeof value === 'number' ? column.format(value) : value}
+        <>
+        {loading &&
+            <CircularProgress />
+        }
+        {!loading &&
+            <Paper className='root'>
+                <TableContainer className='container'>
+                    <Table stickyHeader aria-label='sticky table'>
+                    <TableHead>
+                        <TableRow>
+                        {columns.map(column => (
+                            <TableCell
+                                key={column.id}
+                                align={column.align}
+                                style={{ minWidth: column.minWidth }}
+                            >
+                            {column.label}
                             </TableCell>
-                            );
-                        })}
+                        ))}
                         </TableRow>
-                    );
-                    })}
-                </TableBody>
-                </Table>
-            </TableContainer>
-        </Paper>
+                    </TableHead>
+                    <TableBody>
+                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                        return (
+                            <TableRow hover role='checkbox' tabIndex={-1} key={row.code}>
+                            {columns.map(column => {
+                                const value = row[column.id];
+                                return (
+                                <TableCell key={column.id} align={column.align}>
+                                    {column.format && typeof value === 'number' ? column.format(value) : value}
+                                </TableCell>
+                                );
+                            })}
+                            </TableRow>
+                        );
+                        })}
+                    </TableBody>
+                    </Table>
+                    
+                </TableContainer>
+            </Paper>
+        }
+        
+        </>
     )
 }
 
